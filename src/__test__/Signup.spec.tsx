f22b4ab2c2c32bcecc69d8b70e66b5219acc2f1b
kijiwon <kiji1313@gmail.com>
2025-05-30 16:27:59 +0900

    jest 환경설정 오류 수정

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,10 +1,12 @@
 import "@testing-library/jest-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import SignupPage from "../pages/SignupPage";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({ defaultOptions: {} });
+const queryClient = new QueryClient({
+  defaultOptions: {},
+});
 
 describe("회원가입 테스트", () => {
   test("비밀번호와 비밀번호 확인값이 일치하지 않으면 에러메세지가 표시된다", () => {
@@ -18,6 +20,7 @@ describe("회원가입 테스트", () => {
       },
     ];
 
+    // 메모리라우터 사용
     const router = createMemoryRouter(routes, {
       initialEntries: ["/signup"],
       initialIndex: 0,
