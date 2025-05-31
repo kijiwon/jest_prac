cd436f9d0ac3da839bac785eb6c4c77c7f051d91
kijiwon <kiji1313@gmail.com>
2025-06-01 00:06:56 +0900

    로그인 페이지 그리기

--- src/__test__/Login.spec.tsx
+++ src/__test__/Login.spec.tsx
@@ -1,5 +1,35 @@
 import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LoginPage from "pages/LoginPage";
+
+const queryClient = new QueryClient({
+  defaultOptions: {},
+});
 
 describe("로그인 테스트", () => {
+  beforeEach(() => {
+    // given - 로그인 페이지가 그려짐
+    const routes = [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ];
+
+    // 메모리라우터 사용
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+      initialIndex: 0,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+  });
+
   test("로그인에 실패하면 에러메세지가 나타난다.", () => {});
 });
