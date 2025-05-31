a8ce93320d7ce9146fe9586ceace53839c91f981
kijiwon <kiji1313@gmail.com>
2025-06-01 00:26:52 +0900

    tanstack-query test

--- src/__test__/Login.spec.tsx
+++ src/__test__/Login.spec.tsx
@@ -1,15 +1,16 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, renderHook, waitFor } from "@testing-library/react";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import LoginPage from "pages/LoginPage";
+import LoginPage from "../pages/LoginPage";
+import useLogin from "../hooks/useLogin";
 
 const queryClient = new QueryClient({
   defaultOptions: {},
 });
 
 describe("로그인 테스트", () => {
-  beforeEach(() => {
+  test("로그인에 실패하면 에러메세지가 나타난다.", async () => {
     // given - 로그인 페이지가 그려짐
     const routes = [
       {
@@ -29,7 +30,13 @@ describe("로그인 테스트", () => {
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
-  });
 
-  test("로그인에 실패하면 에러메세지가 나타난다.", () => {});
+    const wrapper = ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
+
+    const { result } = renderHook(() => useLogin(), { wrapper });
+
+    await waitFor(() => expect(result.current.isError).toBe(true));
+  });
 });
